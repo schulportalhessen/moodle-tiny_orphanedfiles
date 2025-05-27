d32c9e5f36407c8b0ac7b2962bc5b9f7529439a0
Andreas Schenkel <Andreas.Schenkel@schulportal.hessen.de>
2025-05-27 14:27:25 +0200

    bump version for moodle 5

--- version.php
+++ version.php
@@ -30,6 +30,6 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version   = 2025052001;
 $plugin->requires  = 2022111800;
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release   = '2.0.0';
+$plugin->release   = '2.0.1';
 $plugin->component = 'tiny_orphanedfiles';
 $plugin->supported = [401, 500];
